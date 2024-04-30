a87fe1af93ed24367d2c956485f07709208d71a7
RPRicardo <163183094+RPRicardo@users.noreply.github.com>
2024-04-30 14:25:55 -0600

    Circulo.java

--- Circulo.java
+++ Circulo.java
@@ -2,9 +2,9 @@ public class Circulo{
     private double radio;
     private String color;
     
-    public Circulo (float radio, String color){
+    public Circulo (double radio, String color){
         this.radio=radio;
-        this.radio=radio;
+        this.color=color;
     }
     public String getColor(){
         return color;
@@ -16,7 +16,7 @@ public class Circulo{
         return 3.141592265 *2 *radio;
     }
      public String toString() {
-        return "Circulo[radius=" + radio + ", color=" + color + "]";
+        return "Circulo[radio=" + radio + ", color=" + color + "]";
     }
      public static void main(String[] args) {
         Circulo circulo1 = new Circulo(4, "rojo");
